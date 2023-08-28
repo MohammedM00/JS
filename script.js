2f50049edcacbe27920164c9bfe2e87a0c22a583
MohammedM00 <131919490+MohammedM00@users.noreply.github.com>
2023-08-27 21:12:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+  const languageInput = document.getElementById("languageInput");
+
+  languageInput.addEventListener("change", function() {
+    const userInput = languageInput.value;
+    alert("You typed: " + userInput);
+  });
+</script>
+
---
+++ script2.js
@@ -0,0 +1,4 @@
+<script>
+  const preferredLibrary = prompt("Which code library do you prefer? Vue, React, or jQuery?");
+  alert("Your preferred code library: " + preferredLibrary);
+</script>
